840e5403b64355ceaf259cb21b6051dd97e3f5b9
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-10 11:50:19 +0530

    Add files via upload

---
+++ Printingawordinasentence.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Printingawordinasentence{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println(a);
+        String b=sc.nextLine();
+        System.out.println(b);
+    }
+}
