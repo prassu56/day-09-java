4ce6361d7ae2e598976af313d5f2094cd36e2703
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-10 16:18:53 +0530

    Add files via upload

---
+++ Mostfrequentindex.java
@@ -0,0 +1,26 @@
+/*
+most repeated letter in a given string*/
+import java.util.*;
+public class Mostfrequentindex
+{
+    public static void main(String[] args)
+    {
+        String a="NAVADEEPAK";
+        int c[]=new int[26];
+        for(int i=0;i<a.length();i++)
+        {
+            c[((int)(a.charAt(i)))-65]++;
+        }
+        int max=0;
+        int index=0;
+        for(int i=0;i<26;i++)
+        {
+            if(c[i]>max)
+            {
+            max=c[i];
+            index=i;
+            }
+        }
+        System.out.println((char)(index+65));
+    }
+}
