ffc59400a4568132a8c4dabcd68f8d493aa0aa90
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-10 10:16:19 +0530

    Add files via upload

---
+++ Printinghelloinstring.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Printinghelloinstring{
+    public static void main(String[]args){
+        String a="Hello";
+        String b=new String("Hai");
+        System.out.println(a);
+          System.out.println(b);
+
+    }
+}
