853af4ec171c780af1ceac744b0d1562a7c47594
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-10 16:07:05 +0530

    Add files via upload

---
+++ Checkuppercaseorlowercase.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Checkuppercaseorlowercase{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        char s=sc.next().charAt(0);
+        if((s&32)==0)
+        System.out.println("Upper Case");
+        else
+        System.out.println("Lower Case");    
+        }
+}
